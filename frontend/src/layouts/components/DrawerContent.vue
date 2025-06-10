06fd366aa67a12714b69e1d016057eccf95a83a6
munseunghwan <114590806+letsgiiiiit@users.noreply.github.com>
2025-06-10 13:39:43 +0900

    commit

---
+++ frontend/src/layouts/components/DrawerContent.vue
@@ -0,0 +1,11 @@
+<script setup>
+import {
+  VerticalNavLink,
+  VerticalNavSectionTitle,
+} from '@layouts'
+</script>
+
+<template>
+    <ul>
+    </ul>
+</template>
---
+++ frontend/src/router/index.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: '/',
+      component: () => import('../components/pages/Index.vue'),
+    },
+  ],
+})
+
+export default router;
